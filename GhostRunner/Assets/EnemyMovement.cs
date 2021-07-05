d5a2d3327b553f80681e331d470504970ffaa917
Jovan <radulovic.jovan.ni@gmail.com>
2021-07-05 05:20:36 +0200

    Start

---
+++ GhostRunner/Assets/EnemyMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public Transform target;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
